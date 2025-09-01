e91bdcf038fbe1496e1d50549e9b6bb48f21ce3c
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2025-09-01 22:32:53 +0530

    Add StringsCompare program to compare two user-input names and display if they are the same or different

---
+++ StringsCompare.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class StringsCompare {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner (System.in);
+        // Comparing two Strings
+        System.out.println("Nmae of first person: ");
+        String name1 = scanner.nextLine();
+        System.out.println("Name of second person: ");
+        String name2 = scanner.nextLine();
+        //Syntax for comparing two Strings
+        if(name1.equals(name2)){
+            System.out.println("Both name are same");
+        } else {
+            System.out.println("Both names are different");
+        }
+        // this checks 3 cases 
+        // 1. if both name are same it returns value 0
+        //2. if name1 > name2 it returns positive value 
+        //3.if name1< name2 it returns negative value 
+
+        }
+
+    }
+
