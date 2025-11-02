64a912b400d27016e0f68a9917725c3f3bc8e926
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2025-11-02 21:13:21 +0530

    Add Substring2 java program

---
+++ SubString2.java
@@ -0,0 +1,8 @@
+public class SubString2 {
+    public static void main(String args[]){
+        String sentence = "My name is Henry Cavil and I play Superman";
+        String name = sentence.substring(11, sentence.length());
+        System.out.println(name);
+    }
+    
+}
