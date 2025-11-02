5462a66606c32175e382ee03f1453e4a72eb8d9b
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2025-11-02 21:06:56 +0530

    Addes Substring java program

---
+++ SubString.java
@@ -0,0 +1,8 @@
+public class SubString {
+    public static void main(String args[]) {
+        String sentence = "I am SuperMan";
+        // substring(beginning indes, ending index)
+        String name = sentence.substring(4, 13);
+         System.out.println(name);
+    }
+}
