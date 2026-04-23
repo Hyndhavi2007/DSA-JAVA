907e8460b20aefc3024ce2bebb9e6a0dd9f3f5bb
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-04-23 10:03:33 +0530

    Add second largest element

--- Second_Largest_Element.java
+++ Second_Largest_Element.java
@@ -8,8 +8,8 @@ public static void main (String [] args){
         max1 = arr[0];
         max2 = arr[1];
     } else {
-        max1 = arr[0];
-        max2 = arr[1];
+        max1 = arr[1];
+        max2 = arr[0];
     }
     for(int i = 2; i<n;i++){
         if(arr[i] > max1){
