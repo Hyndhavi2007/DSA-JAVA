5430da5b69a187a37de75b2749d82f6e40ba4206
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-05-29 22:59:01 +0530

    Add Student java code using arrays

---
+++ Student.java
@@ -0,0 +1,31 @@
+public class Student {
+    int rollno;
+    String name;
+    int marks;
+
+    
+        public static void main(String[]args){
+            Student s1 = new Student();
+            s1.rollno = 1;
+            s1.name = "Max";
+            s1.marks = 99;
+
+            Student s2 = new Student();
+            s2.rollno = 2;
+            s2.name = "Alex";
+            s2.marks = 90;
+
+            Student s3 = new Student();
+            s3.rollno = 3;
+            s3.name = "George";
+            s3.marks = 89;
+            Student students[] = new Student[3];
+            students[0] =s1;
+            students[1] = s2;
+            students[2] = s3;
+            for(int i =0 ; i< students.length;i++){
+                System.out.println(students[i].name + ": " + students[i].marks);
+            }
+        }
+    }
+    
