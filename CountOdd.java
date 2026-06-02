d00157a42b3cb0474a42dfffd7cb5dba32073969
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-02 21:55:44 +0530

    Add Odd count java code using enhanced for loop

---
+++ CountOdd.java
@@ -0,0 +1,17 @@
+public class CountOdd {
+    public static void main(String[]args){
+        int nums[] = new int[10];
+         int count = 0;
+        for(int i = 0; i < nums.length;i++){
+            nums[i] = (int)(Math.random() * 100);
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+        for(int n : nums){
+            if(n % 2 != 0){
+                count++;
+            }
+        }
+        System.out.println("The number of the odd numbers in the array is " + count);
+    }
+}
