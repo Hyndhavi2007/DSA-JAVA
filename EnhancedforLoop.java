624db93d45885e6ab162f34750184269147ea45f
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-02 21:22:27 +0530

    add enhanced for loop java cpde

---
+++ EnhancedforLoop.java
@@ -0,0 +1,8 @@
+public class EnhancedforLoop {
+    public static void main (String [] args){
+        int[] nums = {10,20,30,40,50};
+        for(int n : nums){
+            System.out.println(n);
+        }
+    }
+}
